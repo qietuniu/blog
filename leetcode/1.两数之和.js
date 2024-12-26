127270868f81cb4fc7aab612f597e16f4a6bd313
chenting1 <chenting1@xiaoyouzi.com>
2024-12-26 10:01:36 +0800

    docs: 注释

--- leetcode/1.两数之和.js
+++ leetcode/1.两数之和.js
@@ -10,8 +10,8 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
+var twoSum = function (nums, target) {
+    console.log(nums)
 };
 // @lc code=end
 
